096d2df4f5928391dd457661b499ea2759864f59
damienchangenot <damien.changenot@gmail.com>
2021-02-03 20:55:58 +0100

    finish part actuality

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualityRepository;
 use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +13,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_index")
      */
-    public function index(StudentRepository $studentRepository): Response
+    public function index(StudentRepository $studentRepository, ActualityRepository $actualityRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'students' => $studentRepository->findBy([], ['id' => 'DESC'], 3),
+            'actualities' => $actualityRepository->findBy([], ['id' => 'DESC'], 3),
         ]);
     }
 }
